515868c96a99e254d6e461c7635064e76bdb264d
ruthwik21ch <chruthwik3@gmail.com>
2026-05-11 13:56:35 +0530

    Enhance Vite config for emailjs support
    Added optimization and build configuration for emailjs.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
   ],
+  optimizeDeps: {
+    include: ['@emailjs/browser'],
+  },
+  build: {
+    commonjsOptions: {
+      include: [/@emailjs\/browser/, /node_modules/],
+    },
+  },
 })
